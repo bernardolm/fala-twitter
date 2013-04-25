34eda04d48fbef8766660c538ba22f2133fb8135
Bernardo Loureiro <bernardo.lou@gmail.com>
2013-04-24 21:08:12 -0300

    add audio event

--- js/fala.js
+++ js/fala.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
 	var textoInicio = 'RT @Rodriggo_f: o q acontece na rod. Washington luiz? Parada mto antes do carrefour. reboques da Prefeitura na R. Morais e silva, Tijuca. Dps o cidadão notícia como furto e danou-se!';
-
 	$('.texto-inicio').text(textoInicio);
 
-	var textoPartes = texto.match(/.{0,99}/g);
+	var textoTraduzido = traduzir(textoInicio);
+	$('.texto-alterado').text(textoTraduzido);
 
-	for (var texto in textoPartes) {
+	var textoPartes = textoTraduzido.match(/.{0,99}/g);
 
-		var textoAlterado = traduzir(texto);
-		
-		$('.texto-alterado').text(textoAlterado);
+	var playNext = function () {
+    	console.log('acabou');
+    }
 
-		var audioURL = "http://translate.google.com/translate_tts?ie=UTF-8&tl=pt&q=" + textoAlterado.replace(/ /g, '+');
+	var audio = new Audio();
+    //audio.src = "http://translate.google.com/translate_tts?ie=UTF-8&tl=pt-BR&q=" + textoPartes[0].replace(/ /g, '+');
+    audio.src = "http://translate.google.com/translate_tts?ie=UTF-8&tl=pt&q=oi+voc%C3%AA+a%C3%AD";
+	audio.addEventListener('ended', playNext);
+	audio.play();
+    
+	/*for (var texto in textoPartes) {
+		var audioURL = "http://translate.google.com/translate_tts?ie=UTF-8&tl=pt-BR&q=" + textoTraduzido.replace(/ /g, '+');
 
 		$('.audio-url').text(audioURL);
 
 		$('audio source').attr('src', audioURL);
-	}
+	}*/
 });
 
 var traduzir = function (texto) {
